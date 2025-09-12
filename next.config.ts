9e5ac5e6d32d07e5ac791378804b418e2d10cc28
Bismark Arroyo <bis.arroyo@gmail.com>
2025-09-12 17:11:05 -0600

    add cloudinary to accepted images

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'hhcvyixkrbfpsfaotugn.supabase.co',
-        port: '',
-        pathname: '/**',
-        search: ''
-      }
-    ]
-  }
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/**',
+                search: ''
+            }
+        ]
+    }
 }
 
 export default nextConfig
